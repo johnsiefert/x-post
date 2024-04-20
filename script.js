31816031b77e3cdedd4d22873dcbae32adf9131c
John <johnsief12@gmail.com>
2024-04-20 17:47:11 -0400

    first commit and set up files

---
+++ script.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js';
+import {
+  getDatabase,
+  ref,
+  push,
+  onValue,
+  remove,
+} from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js';
+
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const postInDB = ref(database, 'Post');
